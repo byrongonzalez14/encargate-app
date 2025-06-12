d9b654d77435e70e29be62100960263d2d3ed961
byrongonzalez14 <byrongonzalez14@gmail.com>
2025-06-12 10:00:03 -0500

    fix: Footer logos

--- src/sections/FeatureSection.vue
+++ src/sections/FeatureSection.vue
@@ -37,6 +37,31 @@ let isOpen = ref(1);
                 </ul>
             </div>
 
+            <!-- Contenido del tab -->
+      <div ref="featuresSection" class="mt-16">
+        <template v-for="feature in features" :key="feature.id">
+          <div v-show="isOpen === feature.id" class="grid gap-32 lg:grid-cols-2 items-center">
+            <div class="relative">
+              <img class="z-10 w-full" :src="feature.details.imageUrl" />
+              <div
+                class="-z-10 bg-theme-primary h-52 w-96 sm:h-80 sm:w-full rounded-r-full absolute -left-56 -bottom-16"
+              ></div>
+            </div>
+            <div>
+              <h3 class="font-theme-heading text-2xl md:text-3xl font-medium text-center lg:text-left">
+                {{ feature.details.title }}
+              </h3>
+              <p class="mt-7 font-theme-content text-lg text-theme-grayish-blue text-center lg:text-left">
+                {{ feature.details.description }}
+              </p>
+              <div class="flex justify-center lg:justify-start mt-7">
+                <LinkButton btn-type="primary" :link="feature.details.link">Más información</LinkButton>
+              </div>
+            </div>
+          </div>
+        </template>
+      </div>
+
             <div ref="featuresSection" class="mt-16">
                 <!-- Tab Content -->
                 <template v-for="feature in features" :key="feature.id">
