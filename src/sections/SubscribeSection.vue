1b25e8f3299a62f7ca94a84b23e6fe7ce660575c
byrongonzalez14 <byrongonzalez14@gmail.com>
2025-09-29 14:48:51 -0500

    Fix: change API URL to production server

--- src/sections/SubscribeSection.vue
+++ src/sections/SubscribeSection.vue
@@ -40,7 +40,7 @@ let handleSubmit = async () => {
 
     if (!hasErrors) {
         try {
-            const response = await fetch("http://localhost:3001/subscribe", {
+            const response = await fetch("https://encargate-api.onrender.com/subscribe", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
